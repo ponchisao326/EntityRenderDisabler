b824d76a913a1677ec19dbf663c500997fcbdd89
ponchisao326 <ponchisao326@hotmail.com>
2024-12-13 20:36:51 +0100

    Added YACL Config Object

--- src/main/java/victorgponce/com/entityrenderdisabler/client/EntityrenderdisablerClient.java
+++ src/main/java/victorgponce/com/entityrenderdisabler/client/EntityrenderdisablerClient.java
@@ -1,10 +1,48 @@
-package victorgponce.com.entityrenderdisabler.client;
+    package victorgponce.com.entityrenderdisabler.client;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.minecraft.client.render.entity.EntityRenderer;
+import net.minecraft.client.render.entity.EntityRendererFactory;
+import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityType;
+import net.minecraft.util.Identifier;
+import org.slf4j.Logger;
+import net.fabricmc.fabric.api.client.rendering.v1.EntityRendererRegistry;
+import org.slf4j.LoggerFactory;
+import victorgponce.com.entityrenderdisabler.config.ConfigHandler;
 
-public class EntityrenderdisablerClient implements ClientModInitializer {
+    public class EntityrenderdisablerClient implements ClientModInitializer {
+
+    public static final String MOD_ID_CLIENT = "entityrenderdisabler";
+    public static final Logger LOGGER_CLIENT = LoggerFactory.getLogger(MOD_ID_CLIENT);
 
     @Override
     public void onInitializeClient() {
+        LOGGER_CLIENT.info("[Entity Render Disabler] The mod is being initialized");
+        LOGGER_CLIENT.info("[Entity Render Disabler] Created by Ponchisao326");
+
+        // Cargamos la configuración
+        ConfigHandler.loadConfig();
+
+        // Se registra el piglin
+        EntityRendererRegistry.register(EntityType.PIGLIN, NoRenderEntityRenderer::new);
+    }
+
+    private static class NoRenderEntityRenderer<T extends Entity> extends EntityRenderer<T> {
+        protected NoRenderEntityRenderer(EntityRendererFactory.Context context) {
+            super(context);
+        }
+
+        @Override
+        public void render(T entity, float yaw, float tickDelta, MatrixStack matrices, net.minecraft.client.render.VertexConsumerProvider vertexConsumers, int light) {
+            // Se deshabilida el render.
+
+        }
+
+        @Override
+        public Identifier getTexture(T entity) {
+            return null; // Quitamos la textura pues no se renderiza.
+        }
     }
 }
--- src/main/java/victorgponce/com/entityrenderdisabler/config/ConfigHandler.java
+++ src/main/java/victorgponce/com/entityrenderdisabler/config/ConfigHandler.java
@@ -1,4 +1,37 @@
 package victorgponce.com.entityrenderdisabler.config;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class ConfigHandler {
+    private static final File CONFIG_FILE = new File("config/entity_render_disabler.json");
+    private static final Gson GSON = new GsonBuilder().setPrettyPrinting().create();
+
+    public static ModMenuIntegration config = new ModMenuIntegration();
+
+    public static void loadConfig() {
+        if (CONFIG_FILE.exists()) {
+            try (FileReader reader = new FileReader(CONFIG_FILE)) {
+                config = GSON.fromJson(reader, ModMenuIntegration.class);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } else {
+            saveConfig(); // Crear archivo con valores por defecto
+        }
+    }
+
+
+    public static void saveConfig() {
+        try (FileWriter writer = new FileWriter(CONFIG_FILE)) {
+            GSON.toJson(config, writer);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
