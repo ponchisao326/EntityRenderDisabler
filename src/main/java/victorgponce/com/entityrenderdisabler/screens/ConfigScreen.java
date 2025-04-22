7053bafb5bbbcfd684dcb0908ac0b58c70d2a68f
Ponchisao3262 <ponchisao326@digitechfp.com>
2025-04-22 14:11:17 +0200

    Started new config Screen

---
+++ src/main/java/victorgponce/com/entityrenderdisabler/screens/ConfigScreen.java
@@ -0,0 +1,29 @@
+package victorgponce.com.entityrenderdisabler.screens;
+
+import com.mojang.blaze3d.systems.RenderSystem;
+import net.minecraft.client.gui.DrawContext;
+import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.text.Text;
+import net.minecraft.util.Identifier;
+
+public class ConfigScreen extends Screen {
+
+    private final Identifier background = Identifier.of("entityrenderdisabler:textures/background.png");
+
+    public ConfigScreen() {
+        super(Text.literal("Entity Render Disabler Configuration"));
+    }
+
+    @Override
+    protected void init() {
+        super.init();
+    }
+
+    @Override
+    public void render(DrawContext context, int mouseX, int mouseY, float delta) {
+        RenderSystem.setShaderTexture(0, background);
+        context.drawTexture(background, 0, 0, 0F, 0F, this.width, this.height, this.width, this.height);
+
+        super.render(context, mouseX, mouseY, delta);
+    }
+}
